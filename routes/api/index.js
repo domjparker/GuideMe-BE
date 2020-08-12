02990b6ed88db0cd2e8a54f74d480f5d14bf569c
MegaGrega <mkmgrega@gmail.com>
2020-08-11 23:14:24 -0700

    small fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const adventureRoutes = require("./adventure");
+const userRoutes = require("./user");
 
 // Book routes
 router.use("/adventure", adventureRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
