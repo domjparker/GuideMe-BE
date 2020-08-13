d5f9f596ab4865843af748587f02f69f3e8496db
Rees Dart <thedominicparker@gmail.com>
2020-08-13 13:26:14 -0700

    added CORS dependency backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,64 @@
 const db = require("../models")
+const bcrypt = require("bcrypt")
 
 module.exports = {
-    findById: function(req,res) {
-        db.User
-        .findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+    findById: function (req, res) {
+        db.User.findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
-    create: function(req,res) {
-        db.User
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+    signup: function (req, res) {
+        db.User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password
+        }).then(dbModel => res.json(dbModel.id))
+            .catch(err => res.status(422).json(err));
     },
-    update: function(req,res) {
-        db.User
-        .findOneAndUpdate({ _id: req.params.id }, req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+
+    login: function (req, res) {
+        db.User.findOne({
+            email: req.body.email
+        }).then(data => {
+            if (!data) {
+                return res.status(404).send('no such user')
+            } else {
+                if (bcrypt, bcrypt.compareSync(req.body.password === user.password)) {
+                    req.session.user = {
+                        _id: data._id,
+                        email: data.email
+                    }
+                    console.log(req.session.user)
+                    res.send("login successful");
+                } else {
+                    res.status(401).send("wrong password")
+                }
+            }
+        }).catch(err => {
+            return res.status(500).end();
+        });
     },
-    remove: function(req, res) {
-        db.User
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+
+    update: function (req, res) {
+        // TODO: this needs to be checked. remove TODO when checked.
+        db.User.findOneAndUpdate({ _id: req.params.id }, {
+            bio: req.body.bio,
+            location: req.body.location,
+            profilePicture: req.body.profile.Picture,
+            bannerPicture: req.body.bannerPicture,
+            tags: req.body.tags,
+            host: req.body.host
+        })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+
+    remove: function (req, res) {
+        db.User.findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 };
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
+// * Server Setup
+
+// Dependencies
 const express = require("express");
+const cors=require('cors');
+// const session = require("express-session")
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3001;
-
+// Express App Setup
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(logger("dev"));
 
+// Sets up the Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(cors({
+  origin:["http://localhost:3000"],
+  credentials:true
+}))
 
+// Session Setup
+// app.use(session({
+//   secret: "dimma stealth",
+
+// }))
+
+// Connects with MongoDB via Mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/guideme", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -22,6 +38,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/guideme", {
 // routes
 app.use(routes);
 
+// Starts Express Server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
