ddec6d5d27290d96c9b1405b9e82e5e48a04f3be
MegaGrega <mkmgrega@gmail.com>
2020-08-13 12:55:47 -0700

    removed update from tagController

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -13,12 +13,6 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
-    update: function(req,res) {
-        db.Tag
-        .findOneAndUpdate({ _id: req.params.id }, {$push:{tags:[req.body]}})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
     remove: function(req, res) {
         db.Tag
         .findById({ _id: req.params.id })
