e4c9550dddc762e4a52966c1c364ba165eeb397d
MegaGrega <mkmgrega@gmail.com>
2020-08-13 12:43:55 -0700

    updating no longer puts empty tag into array

--- routes/api/adventure.js
+++ routes/api/adventure.js
@@ -5,18 +5,21 @@ const adventureController = require("../../controllers/adventureController");
 router.route("/")
   .get(adventureController.findAll)
   .post(adventureController.create);
-
+// Matches with "/api/adventure/:id"
 router
   .route("/:id")
   .get(adventureController.findById)
   .put(adventureController.update)
   .delete(adventureController.remove);
+// Matches with "/api/adventure/location/:location"  
 router
   .route("/location/:location")
   .get(adventureController.findLocation);
+// Matches with "/api/adventure/tags/:tag"
 router
   .route("/tags/:tag")
   .get(adventureController.findTag)
+  // Matches with "/api/adventure/host/:id"
 router
   .route("/host/:id")
   .get(adventureController.findByHostId)
