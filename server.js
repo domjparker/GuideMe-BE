abcd510a5fd7891acdbeaacde10de8219cdfe784
Matthew Grega <mkmgrega@gmail.com>
2020-08-17 11:44:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-  origin:["http://localhost:3000"],
+  origin:["https://guidemedimma.herokuapp.com"],
   credentials:true
 }));
 
