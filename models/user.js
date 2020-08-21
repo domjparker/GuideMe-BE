49aee26e709040dc15759d080b73f8880da444c8
MegaGrega <mkmgrega@gmail.com>
2020-08-21 00:28:08 -0700

    Added availability to User Model

--- models/user.js
+++ models/user.js
@@ -40,6 +40,17 @@ const userSchema = new Schema({
       lastChange: { type: Date, default: Date.now },
       
     }
+  ],
+  availability: [
+    {
+      startDate: {type:Date},
+      endDate: {type:Date},
+      timeZone:{type:String},
+      adventureId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      }
+    }
   ]
 });
 
