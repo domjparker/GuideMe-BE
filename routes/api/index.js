b5bb1cb90b280113ef7ef530a3d8614437b87451
Rees Dart <thedominicparker@gmail.com>
2020-08-22 16:00:55 -0700

    added community model, controller, and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const adventureRoutes = require("./adventure");
 const userRoutes = require("./user");
 const messageRoutes = require("./message");
 const tagRoutes = require("./tag");
+const communityRoutes = require("./community");
 
 const { route } = require("./adventure");
 
@@ -11,6 +12,8 @@ router.use("/adventure", adventureRoutes);
 router.use("/user", userRoutes);
 router.use("/message", messageRoutes);
 router.use("/tag", tagRoutes);
+router.use("/community", communityRoutes);
+
 
 
 module.exports = router;
