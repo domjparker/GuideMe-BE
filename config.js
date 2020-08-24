393cb6ded28630c3bf4699d3545ac8397c9c0c46
Rees Dart <thedominicparker@gmail.com>
2020-08-24 15:28:17 -0700

    fixed merge error

--- config.js
+++ config.js
@@ -3,12 +3,3 @@ module.exports = {
     PASS: 'GuideMeApp2020$',
     SESSIONSECRET: "dimma stealth"
 }
-
-// for server
-const creds = require('../config');
-
-secret: creds.SESSIONSECRET,
-
-
-
-// add config.js to gitignore file
